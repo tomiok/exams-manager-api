44f7d37288ea84e4e7d1a0e1b4988504ff2a8fef
Tomas Lingotti <tomas.lingotti@globant.com>
2018-01-24 10:16:09 -0300

    first commit

---
+++ mongo-first-demo/src/main/java/org/tommy/mongofirstdemo/MongoFirstDemoApplication.java
@@ -0,0 +1,12 @@
+package org.tommy.mongofirstdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoFirstDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoFirstDemoApplication.class, args);
+	}
+}
