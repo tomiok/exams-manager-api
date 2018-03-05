85ca21f55eab51a472eb1009e72a182e6e7d01cf
Tomas Lingotti <tomaslingotti@hotmail.com>
2018-03-04 21:57:35 -0300

    Next rc (#3)
    * update spring boot version 2 RELEASE
    
    * Feature register student with facebook (#2)
    
    * Add cipher service and update student model
    
    * Better exception handling in cipher service
    
    * finish flow to add new students {but not facebook} and some other little changes in yml file

---
+++ mongo-first-demo/src/main/java/org/tommy/mongofirstdemo/component/student/StudentEntityGateway.java
@@ -0,0 +1,33 @@
+package org.tommy.mongofirstdemo.component.student;
+
+import static org.tommy.mongofirstdemo.component.student.StudentComponent.StudentResponse.fromModel;
+
+import java.util.Optional;
+import org.tommy.mongofirstdemo.application.cipher.ApplicationCipher;
+import org.tommy.mongofirstdemo.domain.student.Student;
+import org.tommy.mongofirstdemo.domain.student.StudentRepository;
+
+public class StudentEntityGateway {
+
+  private final StudentRepository studentRepository;
+
+  private final ApplicationCipher applicationCipher;
+
+  public StudentEntityGateway(final StudentRepository studentRepository,
+                              final ApplicationCipher applicationCipher) {
+    this.studentRepository = studentRepository;
+    this.applicationCipher = applicationCipher;
+  }
+
+  Student saveStudent(final Student student) {
+    studentRepository.deleteAll(); //TODO remove this sometime in the future
+    Optional<String> pass = Optional.ofNullable(student.getPassword());
+    pass.ifPresent(s -> student.setPassword(applicationCipher.encrypt(s)));
+    return studentRepository.save(student);
+  }
+
+  StudentComponent.StudentResponse findStudentById(final String id) {
+    Student student = studentRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+    return fromModel(student);
+  }
+}
