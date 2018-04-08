1c06d196087f239aa37e79623dffb83f7c539744
Tomas Lingotti <tomaslingotti@hotmail.com>
2018-04-07 21:24:52 -0300

    Feature add jwt (#8)
    * Next rc (#6)
    
    * update spring boot version 2 RELEASE
    
    * Feature register student with facebook (#2)
    
    * Add cipher service and update student model
    
    * Better exception handling in cipher service
    
    * finish flow to add new students {but not facebook} and some other little changes in yml file
    
    * Feature register student with facebook (#4)
    
    * Add cipher service and update student model
    
    * Better exception handling in cipher service
    
    * finish flow to add new students {but not facebook} and some other little changes in yml file
    
    * Add encryption to teacher creation (#5)
    
    * find classes services

--- mongo-first-demo/src/main/java/org/tommy/mongofirstdemo/component/classroom/domain/ClassRepository.java
+++ mongo-first-demo/src/main/java/org/tommy/mongofirstdemo/component/classroom/domain/ClassRepository.java
@@ -1,6 +1,12 @@
 package org.tommy.mongofirstdemo.component.classroom.domain;
 
+import java.util.Set;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ClassRepository extends CrudRepository<ClassRequest, String> {
+
+  Set<ClassRequest> findByStudentId(final String studentId);
+
+  Set<ClassRequest> findByTeacherId(final String teacherId);
 }
+
--- mongo-first-demo/src/main/java/org/tommy/mongofirstdemo/component/classroom/web/ClassController.java
+++ mongo-first-demo/src/main/java/org/tommy/mongofirstdemo/component/classroom/web/ClassController.java
@@ -3,11 +3,14 @@ package org.tommy.mongofirstdemo.component.classroom.web;
 import java.net.URI;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.tommy.mongofirstdemo.component.classroom.domain.ClassRequest;
+import org.tommy.mongofirstdemo.component.classroom.usecase.FindClassService;
 import org.tommy.mongofirstdemo.component.classroom.usecase.RequestClassService;
 import org.tommy.mongofirstdemo.shared.WebUtils;
 
@@ -17,8 +20,12 @@ public class ClassController {
 
   private final RequestClassService requestClassService;
 
-  public ClassController(final RequestClassService requestClassService) {
+  private final FindClassService findClassService;
+
+  ClassController(final RequestClassService requestClassService,
+                  final FindClassService findClassService) {
     this.requestClassService = requestClassService;
+    this.findClassService = findClassService;
   }
 
   @PostMapping
@@ -28,5 +35,10 @@ public class ClassController {
     URI uri = WebUtils.getCreatedEntityUri(classReq.getId(), httpRequest);
 
     return ResponseEntity.created(uri).build();
+  }
+
+  @GetMapping
+  public ResponseEntity<?> getMyClasses(@RequestParam("id") final String id) {
+    return null;
   }
 }
