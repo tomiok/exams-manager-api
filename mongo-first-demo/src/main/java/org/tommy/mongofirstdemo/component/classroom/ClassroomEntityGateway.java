0b7de0261ac34940e671df978165003ad2979f40
tomas lingotti <tomaslingotti{at}gmail.com>
2018-04-14 21:37:31 -0300

    Add logic to verify class requests duplicated

--- mongo-first-demo/src/main/java/org/tommy/mongofirstdemo/component/classroom/ClassroomEntityGateway.java
+++ mongo-first-demo/src/main/java/org/tommy/mongofirstdemo/component/classroom/ClassroomEntityGateway.java
@@ -1,13 +1,19 @@
 package org.tommy.mongofirstdemo.component.classroom;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
 import java.util.Set;
 import org.bson.types.ObjectId;
 import org.tommy.mongofirstdemo.component.classroom.domain.ClassRepository;
 import org.tommy.mongofirstdemo.component.classroom.domain.ClassRequest;
+import org.tommy.mongofirstdemo.component.classroom.domain.DuplicatedClassException;
 
 public class ClassroomEntityGateway {
 
   private ClassRepository classRepository;
+
+  private static final long DAYS = 10;
 
   public ClassroomEntityGateway(final ClassRepository classRepository) {
     this.classRepository = classRepository;
@@ -30,5 +36,17 @@ public class ClassroomEntityGateway {
     ClassRequest classSaved = classRepository
         .findByStudentIdAndTeacherId(new ObjectId(studentId), new ObjectId(teacherId));
 
+    if (!isTooOld(classSaved)) {
+      throw new DuplicatedClassException();
+    }
+  }
+
+  private boolean isTooOld(final ClassRequest classRequest) {
+    return classRequest.getDateCreated().isBefore(getFromDaysBefore());
+  }
+
+  private Instant getFromDaysBefore() {
+    LocalDate localDate = LocalDate.now();
+    return localDate.minusDays(DAYS).atStartOfDay().toInstant(ZoneOffset.UTC);
   }
 }
---
+++ mongo-first-demo/src/main/java/org/tommy/mongofirstdemo/component/classroom/domain/DuplicatedClassException.java
@@ -0,0 +1,11 @@
+package org.tommy.mongofirstdemo.component.classroom.domain;
+
+public class DuplicatedClassException extends RuntimeException {
+
+  private static final String MESSAGE = "This class is duplicated, please cancel the old one or wait until"
+                                        + "the teacher accept yours";
+
+  public DuplicatedClassException() {
+    super(MESSAGE);
+  }
+}
