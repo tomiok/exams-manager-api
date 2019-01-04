17c110398e400b34e00097233a61d99fa45d48ba
Tomas Lingotti <tomaslingotti@hotmail.com>
2019-01-03 21:06:20 -0300

    finish save exam test

--- exams-manager-api/src/test/java/org/tommy/examsmanager/component/exam/usecase/SaveExamServiceImplTest.java
+++ exams-manager-api/src/test/java/org/tommy/examsmanager/component/exam/usecase/SaveExamServiceImplTest.java
@@ -1,12 +1,19 @@
 package org.tommy.examsmanager.component.exam.usecase;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import java.time.LocalDate;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.tommy.examsmanager.component.exam.domain.Exam;
 import org.tommy.examsmanager.component.exam.domain.ExamEntityGateway;
+import org.tommy.examsmanager.component.student.domain.Student;
 import org.tommy.examsmanager.component.student.domain.StudentEntityGateway;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -26,7 +33,22 @@ public class SaveExamServiceImplTest {
 
     String studentId = "someId";
     String signature = "History";
+    LocalDate now = LocalDate.now();
 
+    SaveExamService.SaveExamRequest request =
+        createRequest(studentId, signature, "some comments",
+            false, now.plusDays(9));
+
+    when(examEntityGateway.saveExam(any(Exam.class))).thenReturn(
+        new Exam(signature, now.plusDays(9), "some comments", false));
+
+    when(studentEntityGateway.findById(studentId)).thenReturn(new Student());
+
+    Exam e = saveExamService.saveExam(request);
+
+    assertThat(e.getSignature()).isEqualTo(signature);
+    assertThat(e.getComments()).isEqualTo("some comments");
+    assertThat(e.isEnrolled()).isFalse();
   }
 
   private SaveExamService.SaveExamRequest createRequest(
